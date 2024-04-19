d3ea701ede218c458dad075860eb850d25eb9865
Uma Chandra Sekhara Sarma <chandu06p@gmail.com>
2024-04-19 13:31:28 +0530

    About Update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Styles from './About.module.css'
+import { Link } from 'react-router-dom'
 
 function About() {
   return (
-    <div>About</div>
+    <div id={Styles['wrapper']}>
+    <section id={Styles['section']}>
+    <div id={Styles['main']}>
+      <div id={Styles['container']}>
+        <p>Hello,my name is Uma Chandra Sekhara Sarma Thadepalli.I am FullStack Developer and Data enthusiastic</p>
+        <img src='me.png' alt='Profile Pic'/>
+      </div>
+      <Link to="Uma_Chandra_Sekhara_Sarma_Thadepalli_Resume.pdf" target='_blank'>Download Resume</Link>
+    </div>
+    </section>
+    <hr/>
+    <h1><u>Skills</u></h1>
+    <section id={Styles['skills']}>
+      <p><h4>Frontend:</h4>HTML, CSS, JavaScript, React.js</p>
+      <p><h4>Backend:</h4>Node.js,Express.js</p>
+      <p><h4>Database:</h4>MongoDB</p>
+      <p><h4>DataScience:</h4>Machine Learning</p>
+    </section>
+    </div>
   )
 }
 
